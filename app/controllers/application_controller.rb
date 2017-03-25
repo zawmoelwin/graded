13781dd40777683f2ae1385ef316f7cad2dbdbe4
husseinmaad <hussein.maad@outlook.com>
2017-03-25 15:38:50 -0700

    done with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
-    render plain: "Hello World"
-  end 
+   
+     render "/index_folder/index.html.erb" 
+   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'application#index'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users do 
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end 
 end
