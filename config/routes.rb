c7f4c53610620ffd300b05eb775fcab01858d27e
Zaw Lwin <zawmoelwin@gmail.com>
2017-03-25 18:23:20 -0700

    add routes for full apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'application#index'
-  devise_for :users do 
+  devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
-  end 
+  end
+  resources :test
+  resources :questionbanks
+  get "/takingtest" , to: 'taking#test', as: 'test_taking'
+  get "/users/:id" , to: 'users#show', as: 'user_profile'
 end
